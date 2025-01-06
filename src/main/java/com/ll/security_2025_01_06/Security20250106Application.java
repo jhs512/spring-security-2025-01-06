765cb9b3fa39821708fbc838024c0db0a1328eb8
jhs512 <jangka512@gmail.com>
2025-01-06 11:32:13 +0900

    work

--- src/main/java/com/ll/security_2025_01_06/Security20250106Application.java
+++ src/main/java/com/ll/security_2025_01_06/Security20250106Application.java
@@ -2,8 +2,10 @@ package com.ll.security_2025_01_06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Security20250106Application {
 
 	public static void main(String[] args) {
