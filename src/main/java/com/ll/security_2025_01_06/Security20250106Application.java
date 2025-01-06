2b20648e85d149ae455fdae0292a8407c7b16142
jhs512 <jangka512@gmail.com>
2025-01-06 11:18:14 +0900

    setting

---
+++ src/main/java/com/ll/security_2025_01_06/Security20250106Application.java
@@ -0,0 +1,13 @@
+package com.ll.security_2025_01_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security20250106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security20250106Application.class, args);
+	}
+
+}
